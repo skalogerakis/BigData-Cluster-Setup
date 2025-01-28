e19933d2a14a8ebe3b3862603818b3fcb851da80
Stefanos Kalogerakis <skalogerakis97@gmail.com>
2025-01-28 22:12:18 +0200

    Added cleaning scripts

---
+++ experiment-clean.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+rm -rf /tmp/flink*
+rm -rf /tmp/hadoop*
+rm -rf /tmp/rocksdb*
+rm -rf /media/localdisk/skalogerakis/tmp/hadoop*
+rm -rf /media/localdisk/skalogerakis/tmp/rocks*
+rm -rf /media/localdisk/skalogerakis/tmp/io*
+
+sleep 1
+sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches "
+
+
+
